7b4b62a47a615118601eec9536e75b9ba0a30d68
guilhermearaujom <guilherme.matos@ccc.ufcg.edu.br>
2024-12-24 17:19:24 -0300

    server.ts modified to accept requests for all clentes

--- src/server.ts
+++ src/server.ts
@@ -12,9 +12,7 @@ import cors from 'cors';
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-  origin: 'http://localhost:3001'  
-}));
+app.use(cors());  
 
 // Configurações do Swagger
 const swaggerOptions = {
